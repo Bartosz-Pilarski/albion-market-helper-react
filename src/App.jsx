f3031735a0753c0496d3c22d79f2d962c9e40f90
Bartosz-Pilarski <bpilarski2003@protonmail.com>
2024-04-09 20:24:58 +0200

    Routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { useEffect, useState } from "react"
-import { Routes, Route } from "react-router-dom"
+import { Routes, Route, Navigate } from "react-router-dom"
 
 import Navigation from "./components/Navigation.jsx"
 
 import pricesService from "./services/pricesService.js"
 import Home from "./components/Home.jsx"
+import ResourceView from "./components/ResourceView.jsx"
 
 const App = () => {
   const [prices, setPrices] = useState(null)
@@ -22,7 +23,15 @@ const App = () => {
     <main>
       <Navigation></Navigation>
       <Routes>
-        <Route path="/" element={<Home/>}/>
+        <Route path="/" element={<Home/>} />
+        <Route path="/metal" element={<ResourceView resourceType='Metal' />} />
+        <Route path="/wood"  element={<ResourceView resourceType='Wood'  />} />
+        <Route path="/fiber" element={<ResourceView resourceType='Fiber' />} />
+        <Route path="/stone" element={<ResourceView resourceType='Stone' />} />
+        <Route path="/hide"  element={<ResourceView resourceType='Hide'  />} />
+
+        {/* Default redirect to homepage in case of unknown url*/}
+        <Route path="*" element={<Navigate to={"/"} replace={true} />} />
       </Routes>
     </main>
   )

50d3bb638b386af1e57c1da47dfac5625fc6de92
Bartosz-Pilarski <bpilarski2003@protonmail.com>
2024-04-25 02:06:54 +0200

    Removed strict mode

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './style.scss'
@@ -9,11 +8,9 @@ import { Provider } from 'react-redux'
 import store from './store.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
     <Provider store={store}>
       <Router>
         <App />
       </Router>
     </Provider>
-  </React.StrictMode>,
 )

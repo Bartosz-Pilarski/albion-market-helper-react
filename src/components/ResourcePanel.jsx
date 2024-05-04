d83d9f4e896a10ed1cbda1b1dd7f05f7d7af7497
Bartosz-Pilarski <bpilarski2003@protonmail.com>
2024-05-05 01:08:48 +0200

    ResourcePanel CSS

--- src/components/ResourcePanel.jsx
+++ src/components/ResourcePanel.jsx
@@ -1,43 +1,49 @@
 import { getImageHref } from "../util/images"
 import { translateLocation } from "../util/maps"
+import './ResourcePanel.scss'
 
 /**
  * Panel displaying basic resource info, an icon and the relevant prices
  * @component
  */
-const ResourcePanel = ({ resourceInfo }) => {
+const resourcePanel = ({ resourceInfo }) => {
   return (
-    <div className='resourcePanel' style={{border: '2px solid white', padding: '1rem', margin: '1rem', width: '15rem'}}>
+    <div className='resource-panel'>
       <img 
-        className='resourcePanel-icon' 
+        className='resource-panel-icon' 
         src={getImageHref(resourceInfo.name)} 
         alt="resource icon" 
       />
-      <h1 className='resourcePanel-title'>
-        {resourceInfo.title}
-      </h1>
-      <h2 className='resourcePanel-subtitle'>
-        {resourceInfo.subtitle}
-      </h2>
-      <div className='resourcePanel-prices'>
-        <div className='resourcePanel-prices-sell'>
+      <div className='resource-panel-titles'>
+        <h1 className='resource-panel-title'>
+          {resourceInfo.title}
+        </h1>
+
+        <div className='separator'></div>
+        
+        <h2 className='resource-panel-subtitle'>
+          {resourceInfo.subtitle}
+        </h2>
+      </div>
+      <div className='resource-panel-prices'>
+        <div className='resource-panel-prices-sell'>
           <p> Selling price: </p>
           <ul>
             {resourceInfo.prices.map((price) => <li key={`${price.location}-sell`} className={translateLocation[price.location].classname}> { price.sellPrice > 0 ? price.sellPrice : 'Unavailable' } </li>)}
           </ul>
         </div>
-        <div className='resourcePanel-prices-buy'>
+        <div className='resource-panel-prices-buy'>
           <p> Buying price: </p>
           <ul>
             {resourceInfo.prices.map((price) => <li key={`${price.location}-buy`} className={translateLocation[price.location].classname}> {price.buyPrice > 0 ? price.buyPrice : 'Unavailable'} </li>)}
           </ul>
         </div>
       </div>
-      <div className='resourcePanel-recommended'>
+      <div className='resource-panel-recommended'>
 
       </div>
     </div>
   )
 }
 
-export default ResourcePanel
+export default resourcePanel
